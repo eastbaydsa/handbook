5cdac776a480144e14a9c37cb03ac1419882f46b
Sean Durham <sean@sdurham.net>
2018-08-14 19:32:31 -0700

    Move menu links to data directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import menuLinks from "./data/menuLinks";
 import TopNav from "./sections/TopNav";
 import TOC from "./sections/TOC";
 import Cover from "./sections/Cover";
@@ -9,36 +10,18 @@ import WhySection from "./sections/WhySection";
 import MembershipSection from "./sections/MembershipSection";
 import GetInvolvedSection from "./sections/GetInvolvedSection";
 import StructureSection from "./sections/StructureSection";
-import ConflictSection from "./sections/ConflictSection";
 import DirectorySection from "./sections/DirectorySection";
-
-const links = [
-  { label: "Welcome", to: "/welcome" },
-  { label: "Why are we here?", to: "/why" },
-  { label: "What does it mean to be a member?", to: "/membership" },
-  {
-    label: "What's the organizational structure of the DSA?",
-    to: "/structure"
-  },
-  { label: "What do I do as a member?", to: "/get-involved" },
-  {
-    label: "I have a conflict with someone. What should I do?",
-    to: "/conflict"
-  },
-  { label: "Directory", to: "/directory" }
-];
 
 const Content = () => (
   <div>
-    <TopNav links={links} />
+    <TopNav links={menuLinks} />
     <Cover anchor="" />
-    <TOC anchor="toc" links={links} />
+    <TOC anchor="toc" links={menuLinks} />
     <WelcomeSection anchor="welcome" />
     <WhySection anchor="why" />
     <MembershipSection anchor="membership" />
     <StructureSection anchor="structure" />
     <GetInvolvedSection anchor="get-involved" />
-    <ConflictSection anchor="conflict" />
     <DirectorySection anchor="directory" />
   </div>
 );
---
+++ src/data/menuLinks.js
@@ -0,0 +1,11 @@
+export default [
+  { label: "Welcome", to: "/welcome" },
+  { label: "Why are we here?", to: "/why" },
+  { label: "What does it mean to be a member?", to: "/membership" },
+  {
+    label: "What's the organizational structure of the DSA?",
+    to: "/structure"
+  },
+  { label: "What do I do as a member?", to: "/get-involved" },
+  { label: "Directory", to: "/directory" }
+];
